2e920ff0ee06f879b2bcf24c1a7e10bcbd904784
JonStreet <j-street4@hvcc.edu>
2019-10-12 13:32:27 -0400

    BrickBreaker Assignment 6 Submission
    Jon Street Assignment 6 Submission

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadLevel01()
+    {
+        SceneManager.LoadScene("Level01");
+    }
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+    public void LoadGameOverScene()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
