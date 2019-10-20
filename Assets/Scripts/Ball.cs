0cf4c8072f19b297427a145ee7a37fe95ac8e459
JonStreet <j-street4@hvcc.edu>
2019-10-20 15:50:52 -0400

    BrickBreaker Part 2
    We have now added a playspace and movement to the game

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    [SerializeField] private float velocity = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) == true)
+        {
+            LaunchBall(velocity);
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(gameObject.name + " has collided with " + collision.gameObject.name);
+    }
+
+    private void LaunchBall(float v)
+    {
+        Rigidbody2D myRigidBody2D = GetComponent<Rigidbody2D>();
+
+        myRigidBody2D.velocity = new Vector2(0, v);
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    float screenWidthInUnits;
+    float paddleMinX = 1f;
+    float paddleMaxX = 15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenWidthInUnits = 16;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.MoveWithMouse();
+    }
+
+    public void MoveWithMouse()
+    {
+        Vector2 pos = new Vector2(transform.position.x, transform.position.y);
+
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+
+        pos.x = Mathf.Clamp(mousePosInUnits, paddleMinX, paddleMaxX);
+        transform.position = pos;
+
+    }
+}
